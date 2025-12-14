77bc4eca0ebef9f868abb191013373ebc065a832
Rhydor <115670922+Rhydori@users.noreply.github.com>
2025-12-14 20:03:05 -0300

    change: short Mutex.Lock()

--- pkg/engine/physics.go
+++ pkg/engine/physics.go
@@ -23,38 +23,43 @@ func (p *Physics) ProcessMovement(clients []*session.Client, delta float64, Upda
 		char := client.Char
 
 		char.Mu.Lock()
+		input := *char.Input
+		x, y := char.X, char.Y
+		lx, ly := char.LX, char.LY
+		char.Mu.Unlock()
+
 		dir := helper.Vec2{}
-		if char.Input.Left {
+		if input.Left {
 			dir.X -= 1
 		}
-		if char.Input.Right {
+		if input.Right {
 			dir.X += 1
 		}
-		if char.Input.Up {
+		if input.Up {
 			dir.Y -= 1
 		}
-		if char.Input.Down {
+		if input.Down {
 			dir.Y += 1
 		}
-
 		dir = dir.Normalize()
 
 		moveDist := p.charSpeed * delta
 
-		char.X += dir.X * moveDist
-		char.Y += dir.Y * moveDist
+		nx := x + dir.X*moveDist
+		ny := y + dir.Y*moveDist
 
-		if char.X != char.LX || char.Y != char.LY {
-			msg := []byte(fmt.Sprintf("move|%s|%.2f|%.2f", client.ID, char.X, char.Y))
+		if nx != lx || ny != ly {
+			char.Mu.Lock()
+			char.X, char.Y = nx, ny
+			char.LX, char.LY = nx, ny
+			char.Mu.Unlock()
+
+			msg := []byte(fmt.Sprintf("move|%s|%.2f|%.2f", client.ID, nx, ny))
 			select {
 			case UpdateCh <- msg:
 			default:
 				logs.Warnf("Update Channel is full, dropping update for %s", client.ID)
 			}
-
-			char.LX = char.X
-			char.LY = char.Y
 		}
-		char.Mu.Unlock()
 	}
 }
